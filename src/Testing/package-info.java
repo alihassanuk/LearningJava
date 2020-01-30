b63b24a8a95b445077e886021ba46869d3cea7f2
alihassanuk <alihassanuk@gmail.com>
2020-01-29 20:06:11 -0500

    first commit

---
+++ src/Testing/package-info.java
@@ -0,0 +1,14 @@
+package Testing;
+
+import java.util.Scanner;
+
+public class Techcircle {
+	public static void main(String) [] args) {
+	
+		public static void main(String... args) { 
+			String chair, table = "metal";
+			chair = chair + table; 
+			System.out.println(chair); 
+			} 
+	}
+}
---
+++ src/learningJava/Learningjava.java
@@ -0,0 +1,66 @@
+package learningJava;
+
+public class Learningjava {
+
+	public static void main(String[] args) {
+		
+		
+		String make= "Accord";
+		
+		String model= "Model SE";
+		
+//		make="Accord";
+		
+//		model="Model SE";
+		
+		System.out.println("My next year new car will be "+make +" "+model);
+		
+		String salary_$$;
+		String my1stGoal;
+		int $; 
+		String something9989988$$;
+		int speed = 100;
+		int speeD = 100;
+		byte bytevalue = 127;
+		 long longValue = 1111111111;
+		 long maxVal = Long.MAX_VALUE;
+		 long anotherLongValue = 9255544531L;
+		 System.out.println(maxVal);
+		 char charValue ='a';
+		 
+//		 char[] acharArray = {'\u0057','\u0065','\u006c',
+//					'\u0063','\u006f','\u006d','\u0065',
+//					'\u0074','\u006f','\u0074','\u0065',
+//					'\u0063','\u0068','\u0043','\u0069',
+//					'\u0072','\u0063','\u006c','\u0065'};
+//			System.out.println(acharArray);
+		 
+		  int a = 9;
+		 
+		 float b = 9.9f;
+		 
+		 System.out.println(b);
+		 
+		 byte byteNumber = 127;
+		 short shortValue = 325;
+		 byte newShortValue = (byte)shortValue;
+		 System.out.println(newShortValue);
+		 
+		 int intValue = newShortValue;
+		 System.out.println();;
+
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		
+		
+		
+		
+
+	}
+
+}
